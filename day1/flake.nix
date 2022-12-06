0aa4b498120da6fce202b2e597d63e8151c7dcbd
Jon <dyercode@users.noreply.github.com>
2022-12-05 19:30:55 -0500

    start it.

---
+++ day1/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "advent 2022 day 1";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              ponyc
+            ]
+          };
+        }
+      );
+}
